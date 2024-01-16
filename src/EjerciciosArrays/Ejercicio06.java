5802ace5ab2f603a3a8bee43ea3fc16bed077c3b
joseenriquemg <josee.munoz@iesnervion.es>
2024-01-16 14:37:30 +0100

    Ejercicios tema 4

--- src/EjerciciosArrays/Ejercicio06.java
+++ src/EjerciciosArrays/Ejercicio06.java
@@ -1,5 +1,6 @@
 package EjerciciosArrays;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Ejercicio06 {
@@ -8,17 +9,32 @@ public class Ejercicio06 {
 		
 		int tabla[] = new int[1000];
 		int valor;
-		int busqueda;
+		int contador = 0;
 		
 		Scanner sc = new Scanner(System.in);
 		
 		for (int i = 0; i < tabla.length; i++) {
             tabla[i] = (int) (Math.random() * 100);
 	 }
+		
+		System.out.println("Tabla de numeros aleatorios: " + Arrays.toString(tabla));
+		
 		System.out.println("Escribe un valor que quieras ver si existe en la tabla:");
 		valor = sc.nextInt();
-				
-
+		
+		for (int i = 0; i < tabla.length; i++) {
+			if (valor == tabla[i]) {
+				contador ++;	
+			}
+			
+		}
+		
+			if ( contador != 0) {
+				System.out.println("Su numero se repite un total de " + contador + " veces." );
+			}else if ( contador == 0) {
+				System.out.println("Su numero no se encuentra en la tabla.");
+			}
+			sc.close();
 	}
 
 }
