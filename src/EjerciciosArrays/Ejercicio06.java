cac88573df2e96a3676a61e57ba6748b91a720a6
joseenriquemg <josee.munoz@iesnervion.es>
2024-01-16 14:37:43 +0100

    Ejercicios 4

--- src/EjerciciosArrays/Ejercicio06.java
+++ src/EjerciciosArrays/Ejercicio06.java
@@ -6,35 +6,35 @@ import java.util.Scanner;
 public class Ejercicio06 {
 
 	public static void main(String[] args) {
-		
+
 		int tabla[] = new int[1000];
 		int valor;
 		int contador = 0;
-		
+
 		Scanner sc = new Scanner(System.in);
-		
+
 		for (int i = 0; i < tabla.length; i++) {
-            tabla[i] = (int) (Math.random() * 100);
-	 }
-		
+			tabla[i] = (int) (Math.random() * 100);
+		}
+
 		System.out.println("Tabla de numeros aleatorios: " + Arrays.toString(tabla));
-		
+
 		System.out.println("Escribe un valor que quieras ver si existe en la tabla:");
 		valor = sc.nextInt();
-		
+
 		for (int i = 0; i < tabla.length; i++) {
 			if (valor == tabla[i]) {
-				contador ++;	
+				contador++;
 			}
-			
+
 		}
-		
-			if ( contador != 0) {
-				System.out.println("Su numero se repite un total de " + contador + " veces." );
-			}else if ( contador == 0) {
-				System.out.println("Su numero no se encuentra en la tabla.");
-			}
-			sc.close();
+
+		if (contador != 0) {
+			System.out.println("Su numero se repite un total de " + contador + " veces.");
+		} else if (contador == 0) {
+			System.out.println("Su numero no se encuentra en la tabla.");
+		}
+		sc.close();
 	}
 
 }
--- src/EjerciciosArrays/Ejercicio07.java
+++ src/EjerciciosArrays/Ejercicio07.java
@@ -3,7 +3,7 @@ package EjerciciosArrays;
 public class Ejercicio07 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
 	}
 
